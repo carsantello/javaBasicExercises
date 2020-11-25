80a6a6d6ed3684e1be679b51707e9330ae98a3a1
carSantello - Carlos Sanchez Botello <carlos.sanchez.botello@gmail.com>
2020-11-25 17:14:24 -0600

    Pushing the first solved exercise family related

---
+++ VariablesExercise1.java
@@ -0,0 +1,12 @@
+public class VariablesExercise1 {
+    public static void main(String[] args) {
+        String jefa = "Lupe Hernandez";
+        String sister = "Ale Sanchez";
+        String bro = "Jesus Sanchez";
+        String chief = "Pancho Sanchez";
+
+        System.out.println("Les presento a mi fam, esta compuesta por los jefes: " + chief + " " + jefa + " y mis " +
+                "carnales : " + "she: " + sister + "he: " + bro);
+
+    }
+}
